ffdd5e43a0725b3283be2e035b4a067f98aa698b
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-02 21:38:45 +0200

    DSLR

--- 6-DSLR_Cameras/create_search_urls.py
+++ 6-DSLR_Cameras/create_search_urls.py
@@ -1,11 +1,11 @@
-f = open('search_urls_laptop.txt', 'w+')
-url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&ref=nb_sb_noss_1'
+f = open('search_urls_DSLR.txt', 'w+')
+url = 'https://www.amazon.com/s?k=dslr+cameras&rh=n%3A281052&ref=nb_sb_noss'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,100):
-    url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&page=' + str(i) + '&qid=1630527954&ref=sr_pg_' + str(i)
+for i in range(2,102):
+    url = 'https://www.amazon.com/s?k=dslr+cameras&i=photo&rh=n%3A281052&page=' + str(i) + '&qid=1630610066&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
