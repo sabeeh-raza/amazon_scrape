819baa40565c74950623295707963b711402b185
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-02 10:23:31 +0200

    mouse

--- 4-Mouse/create_search_urls.py
+++ 4-Mouse/create_search_urls.py
@@ -1,11 +1,11 @@
-f = open('search_urls_laptop.txt', 'w+')
-url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&ref=nb_sb_noss_1'
+f = open('search_urls_mouse.txt', 'w')
+url = 'https://www.amazon.com/s?k=mouse&i=computers-intl-ship&ref=nb_sb_noss_1'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,100):
-    url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&page=' + str(i) + '&qid=1630527954&ref=sr_pg_' + str(i)
+for i in range(2,120):
+    url = 'https://www.amazon.com/s?k=mouse&i=computers-intl-ship&page=' + str(i) + '&qid=1630568775&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
