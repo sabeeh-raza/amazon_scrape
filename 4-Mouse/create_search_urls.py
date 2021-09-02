133b9774f320bc64156be8233a0d6559b5468f3e
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-03 01:12:05 +0200

    mouse

--- 4-Mouse/create_search_urls.py
+++ 4-Mouse/create_search_urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls_mouse.txt', 'w')
-url = 'https://www.amazon.com/s?k=mouse&i=computers-intl-ship&ref=nb_sb_noss_1'
+url = 'https://www.amazon.com/s?k=computer+mouse&i=computers-intl-ship&ref=nb_sb_noss_1'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,120):
-    url = 'https://www.amazon.com/s?k=mouse&i=computers-intl-ship&page=' + str(i) + '&qid=1630568775&ref=sr_pg_' + str(i)
+for i in range(2,100):
+    url = 'https://www.amazon.com/s?k=computer+mouse&i=computers-intl-ship&page=' + str(i) + '&qid=1630623937&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
