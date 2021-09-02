bdf827f536afd525eccf1f6a2f46d5498423aee0
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-03 00:09:08 +0200

    headphone

--- 8-Headphones/create_search_urls.py
+++ 8-Headphones/create_search_urls.py
@@ -1,11 +1,11 @@
-f = open('search_urls_laptop.txt', 'w+')
-url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&ref=nb_sb_noss_1'
+f = open('search_urls_headphone.txt', 'w+')
+url = 'https://www.amazon.com/s?k=headphones&i=electronics-intl-ship&ref=nb_sb_noss_2'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,100):
-    url = 'https://www.amazon.com/s?k=laptops&i=computers-intl-ship&page=' + str(i) + '&qid=1630527954&ref=sr_pg_' + str(i)
+for i in range(2,110):
+    url = 'https://www.amazon.com/s?k=headphones&i=electronics-intl-ship&page=' + str(i) + '&qid=1630619120&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
