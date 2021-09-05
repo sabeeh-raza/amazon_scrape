77f705f199ddbb35a6536b922a8d819bc7fd0be2
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-06 00:23:38 +0200

    processor

--- 3-Processor/create_search_urls.py
+++ 3-Processor/create_search_urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls_processor.txt', 'w+')
-url = 'https://www.amazon.com/s?k=processor&rh=n%3A193870011&ref=nb_sb_noss'
+url = 'https://www.amazon.com/s?k=processor&i=computers-intl-ship&ref=nb_sb_noss_1'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,85):
-    url = 'https://www.amazon.com/s?k=processor&i=computers&rh=n%3A193870011&page=' + str(i) + '&qid=1630626110&ref=sr_pg_' + str(i)
+for i in range(2,150):
+    url = 'https://www.amazon.com/s?k=processor&i=computers-intl-ship&page=' + str(i) + '&qid=1630847577&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
