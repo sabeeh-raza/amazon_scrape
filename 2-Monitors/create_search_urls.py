4cc931068fb90d64881a4cebf6ad1a97869a7931
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-06 23:41:53 +0200

    monitor

--- 2-Monitors/create_search_urls.py
+++ 2-Monitors/create_search_urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls_monitor.txt', 'w+')
-url = 'https://www.amazon.com/s?k=monitor&rh=n%3A1292115011&ref=nb_sb_noss'
+url = 'https://www.amazon.com/s?k=monitor&i=computers&ref=nb_sb_noss'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,80):
-    url = 'https://www.amazon.com/s?k=monitor&i=computers&rh=n%3A1292115011&page=' + str(i) + '&qid=1630621898&ref=sr_pg_' + str(i)
+for i in range(2,140):
+    url = 'https://www.amazon.com/s?k=monitor&i=computers&page=' + str(i) + '&qid=1630963398&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
