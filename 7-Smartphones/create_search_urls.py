30d000d819a0e48da703df6aec7c5b1fd4493417
sabeehraza-83 <sabeehraza@hotmail.com>
2021-09-07 11:04:55 +0200

    smartphone

--- 7-Smartphones/create_search_urls.py
+++ 7-Smartphones/create_search_urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls_smartphone.txt', 'w+')
-url = 'https://www.amazon.com/s?k=smartphones&rh=n%3A7072561011&ref=nb_sb_noss'
+url = 'https://www.amazon.com/s?k=smartphones&ref=glow_cls&refresh=7&rh=n%3A7072561011'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,71):
-    url = 'https://www.amazon.com/s?k=smartphones&i=mobile&rh=n%3A7072561011&page=' + str(i) + '&qid=1630611405&ref=sr_pg_' + str(i)
+for i in range(2,107):
+    url = 'https://www.amazon.com/s?k=smartphones&i=mobile&rh=n%3A7072561011&page=' + str(i) + '&qid=1631005149&refresh=7&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
